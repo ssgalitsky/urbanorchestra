5754f8ecf00d7a62f2739a93591cc150a47129a0
Jason Cramer <jason.t.cramer@gmail.com>
2018-01-28 14:30:53 -0500

    Refactor sound processing and add audio snippet

--- audio_process.py
+++ audio_process.py
@@ -2,17 +2,17 @@ import numpy as np
 import librosa
 from embedding import extract_vggish_embedding
 
-def extract_embeddings_from_sound_file(audio_filename, output_sr=44100):
-    audio_data, sr = librosa.load(audio_filename, sr=16000)
+def process_sound_file(audio_filename, sr=44100, top_db=10):
+    audio_data, _ = librosa.load(audio_filename, sr=sr)
     spec_samples = int(16000*(10/1000) * 95 + 16000 * 25 / 1000)
-    embeddings = []
-    for start_idx, end_idx in librosa.effects.split(audio_data, top_db=10):
+    candidates = []
+    for start_idx, end_idx in librosa.effects.split(audio_data, top_db=top_db):
         chunk = audio_data[start_idx:end_idx]
         onsets = librosa.onset.onset_detect(chunk)
         for onset_idx, onset_frame_idx in enumerate(onsets):
             onset_sample_idx = librosa.frames_to_samples(onset_frame_idx)[0]
             if onset_sample_idx < len(onsets) - 1:
-                end_sample_idx = librosa.frames_to_samples(onsets[onset_sample_idx+1], sr=fs)[0]
+                end_sample_idx = librosa.frames_to_samples(onsets[onset_sample_idx+1], sr=sr)[0]
             else:
                 end_sample_idx = float('inf')
 
@@ -28,11 +28,15 @@ def extract_embeddings_from_sound_file(audio_filename, output_sr=44100):
             assert emb.shape == (1, 128)
             emb = emb.reshape((128,))
 
-            emb_item = {
+            output_audio_start_idx = start_idx + onset_sample_idx
+            output_audio_end_idx = start_idx + end_sample_idx
+
+            cand_item = {
                 'embedding': emb,
-                'start_idx': int((start_idx + onset_sample_idx) * output_sr/sr),
-                'end_idx': int((start_idx + end_sample_idx) * output_sr/sr)
+                'audio': output_audio_data[output_audio_start_idx, output_audio_end_idx],
+                'start_idx': output_audio_start_idx,
+                'end_idx': output_audio_end_idx
             }
 
-            embeddings.append(emb_item)
-    return embeddings
+            candidates.append(cand_item)
+    return candidates
